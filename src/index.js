8482127fa884e85c3808a2cce00df6bbad165195
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2019-03-16 17:24:36 +0100

    update 0.2
    added arrows

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import './index.scss';
 import './buttons/buttons.pug';
 import './buttons/buttons.scss';
 import './buttons/buttons.js';
+import './arrows/arrow.pug';
+import './arrows/arrow.scss';
+import './arrows/arrow.js';
