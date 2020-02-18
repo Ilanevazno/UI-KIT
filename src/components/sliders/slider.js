3be45cc611783957d6bcd143a9639f8701d87870
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2020-02-18 04:24:02 +0300

    feat: complected sliders, stages, form elements

--- src/components/sliders/slider.js
+++ src/components/sliders/slider.js
@@ -9,15 +9,39 @@ $('.first-slider').jRange({
 	step: 1,
 	scale: [],
 	format: '%s',
-	width: 710
+	width: 365
 });
 
 $('.second-slider').jRange({
-	from: 0,
-	to: 100,
 	theme: 'theme-mint',
-	step: 25,
-	scale: [0, 25, 50, 75, 100],
-	format: '%s',
-	width: 710,
+    from: 0,
+    to: 100,
+    step: 25,
+    scale: [0, 25, 50, 75, 100],
+    format: '%s',
+    width: 300,
+    showLabels: true,
+    snap: true
 });
+
+
+//fix plugin bug
+$(document).on('DOMContentLoaded', () => {
+	const moveLabels = (target) => {
+		$(document).on('mousemove', function() {
+			let position = $(target).position().left;
+			let offset = $(target).width() / 2;
+			$(target).next().css('left', `${position - offset}px`);
+		})
+	}
+
+	$('.slider-container').each(function(idx, itm) {
+		$(itm).find('.pointer').each(function(idx, itm) {
+			moveLabels(itm);
+
+			$(itm).on('mousedown', function() {
+				moveLabels(itm);
+			});
+		})
+	})
+})
