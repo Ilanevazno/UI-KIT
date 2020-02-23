309a72587f6dc42054ea3b64164a6002c9ca3c2f
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2020-02-23 06:06:38 +0300

    feat: location complected

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -1,109 +1,74 @@
 import './location.scss';
+import 'leaflet';
+import 'leaflet-css';
 
-var myMap;
+class Location {
+    constructor (coords) {
+        this.mymap = L.map('map').setView(coords, 13);
+        this.coords = coords;
+    }
 
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
+    bootstrap () {
+        const $mapContainer = $(this.mymap._container);
+        const $showCurrentPosition = $mapContainer.find('.js-overlay-btn-set-address');
+        const $findMarker = $mapContainer.find('.js-overlay-btn-find-marker');
+        $showCurrentPosition.on('click', this.setNewAddress.bind(this));
+        $findMarker.on('click', this.showCurrentPosition.bind(this));
 
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [37.787509, -122.444838], 
-        zoom: 15,
-        controls: ['geolocationControl']
-    }, {
-        searchControlProvider: 'yandex#search'
+    }
+
+    showCurrentPosition () {
+        this.mymap.panTo(this.coords);
+    }
+
+    setNewAddress () {
+        const requestLatitude = prompt('Введите долготу');
+        const requestLongitude = prompt('Введите ширину');
+        if (requestLatitude && requestLongitude) {
+            this.mymap.setView([requestLatitude, requestLongitude],13);
+            const placeMark = L.icon({
+                iconUrl: 'src/assets/images/marker-icon.png',
+                iconSize: [40, 56],
+                iconAnchor: [22, 94],
+                popupAnchor: [-3, -76],
+                shadowUrl: '',
+                shadowSize: [68, 95],
+                shadowAnchor: [22, 94]
+            });
+            L.marker([requestLatitude, requestLongitude], {icon: placeMark}).addTo(this.mymap);
+        } else {
+            alert('Некорректные значения');
+            return
+        }
+    }
+
+    render () {
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiaWxhbmV2YXpubzExIiwiYSI6ImNrNndsbGR6ZjBkMWUzaG1vZHp0dWZmNGoifQ.hdk2ziw77v-cKLyCc4ghoA'
+        }).addTo(this.mymap);
         
-    });
+        
+        var placeMark = L.icon({
+            iconUrl: 'src/assets/images/marker-icon.png',
+            iconSize: [40, 56],
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76],
+            shadowUrl: '',
+            shadowSize: [68, 95],
+            shadowAnchor: [22, 94]
+        });
+        L.marker([37.791362, -122.414720], {icon: placeMark}).addTo(this.mymap);
+    }
 
-    //Управление контрольной панелью
-
-    const ControlPlayout = ymaps.templateLayoutFactory.createClass([
-        '<div class="control__panel"><p class="control__panel-title">Meet us!</p><p class="control__panel-address">1259  CALIFORNIA ST San Francisco, CA</p></div>',
-    ].join(''));
-
-    const ControlPanel = new ymaps.control.Button({
-        options: {
-            layout: ControlPlayout,
-            maxWidth: [170, 190, 220]
-        }
-    });
-
-    myMap.controls.add(ControlPanel, {
-    });
-
-    //Кнопки на контрольной панели
-
-    const ButtonLayout = ymaps.templateLayoutFactory.createClass([
-        '<img class="control__panel-btn" src="{{ data.image }}">',
-    ].join(''));
-    
-
-    const buttonFullScreen = new ymaps.control.Button({
-        data: {
-            image: 'src/assets/images/location.png'
-        },
-        options: {
-            layout: ButtonLayout,
-            maxWidth: [170, 190, 220]
-        }
-    });
-
-    myMap.controls.add(buttonFullScreen, {
-        position: {
-            bottom: '42px',
-            right: '82px'
-        },
-        right: 5,
-        top: 5
-    });
-
-    const BtnGeoLayout = ymaps.templateLayoutFactory.createClass([
-        '<img class="control__panel-btn" src="{{ data.image }}">',
-    ].join(''));
-
-    const buttonGeo = new ymaps.control.Button({
-        data: {
-            image: 'src/assets/images/pin.png'
-        },
-        options: {
-            layout: BtnGeoLayout,
-            maxWidth: [170, 190, 220]
-        }
-    })
-
-    myMap.controls.add(buttonGeo, {
-        position: {
-            bottom: '42px',
-            right: '42px'
-        },
-        right: 50,
-        top: 5
-    })
-    
-    // Данный класс нужен для отметки на карте
-    const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-
-    //Управление отметкой на карте
-    PlaceMark = new ymaps.Placemark([37.787509, -122.444838], {
-        baloonContent: 'Местоположение',
-        iconContent: '<div class="mark-circle"></div>'
-    }, {
-        iconLayout: 'default#imageWithContent',
-        iconImageHref: 'src/assets/images/mark.png',
-        iconImageSize: [30,42],
-        iconImageOffset: [-5, -38],
-        iconContentOffset: [8, 10],
-        iconContentLayout: MyIconContentLayout
-    });
-
-    myMap.geoObjects
-    .add(PlaceMark)
-
+    init () {
+        this.render();
+        this.bootstrap();
+    }
 }
 
+const widgetLocation = new Location([37.787509, -122.444838]);
+widgetLocation.init();
