80ccc932e9ca70789585a88162b4acc21962d5cf
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2020-02-24 17:11:58 +0300

    feat: refactored respondive

--- src/components/sliders/slider.js
+++ src/components/sliders/slider.js
@@ -2,46 +2,67 @@ import '../../vendor/jRange/jquery.range-min';
 import '../../vendor/jRange/jquery.range.css'
 import './slider.scss';
 
-$('.first-slider').jRange({
+class Slider {
+	constructor (selector, settings) {
+		this.$htmlContainer = $(selector);
+		this.settings = settings;
+	}
+
+	render () {
+		this.$htmlContainer.jRange( this.settings );
+		this.fixRangeOffset();
+	}
+
+	fixRangeOffset () {
+		//fix pointer label offset bug
+		const moveLabels = (target) => {
+			$(document).on('mousemove', function() {
+				let position = $(target).position().left;
+				let offset = $(target).width() / 2;
+				$(target).next().css('left', `${position - offset}px`);
+			})
+		}
+
+		this.$htmlContainer.next().each(function(idx, itm) {
+			$(itm).find('.pointer').each(function(idx, itm) {
+				moveLabels(itm);
+
+				$(itm).on('mousedown', function() {
+					moveLabels(itm);
+				});
+			})
+		})
+	}
+
+	bootstrap () {
+		this.render();
+	}
+}
+
+let sliderWidth = window.innerWidth > 960 ? 360 : 230;
+ 
+
+const orangeSlider = new Slider ('.first-slider', {
 	from: 0,
 	to: 100,
 	theme: 'theme-orange',
 	step: 1,
 	scale: [],
 	format: '%s',
-	width: 365
-});
+	width: sliderWidth
+})
+orangeSlider.bootstrap();
 
-$('.second-slider').jRange({
+const mintSlider = new Slider ('.second-slider', {
 	theme: 'theme-mint',
     from: 0,
     to: 100,
     step: 25,
     scale: [0, 25, 50, 75, 100],
     format: '%s',
-    width: 300,
     showLabels: true,
-    snap: true
-});
-
-
-//fix plugin bug
-$(document).on('DOMContentLoaded', () => {
-	const moveLabels = (target) => {
-		$(document).on('mousemove', function() {
-			let position = $(target).position().left;
-			let offset = $(target).width() / 2;
-			$(target).next().css('left', `${position - offset}px`);
-		})
-	}
-
-	$('.slider-container').each(function(idx, itm) {
-		$(itm).find('.pointer').each(function(idx, itm) {
-			moveLabels(itm);
-
-			$(itm).on('mousedown', function() {
-				moveLabels(itm);
-			});
-		})
-	})
+	snap: true,
+	width: sliderWidth
 })
+mintSlider.bootstrap();
+
