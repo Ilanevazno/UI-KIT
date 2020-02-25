858c971ac981ba1a133c7bb6314d6825c24629d0
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2020-02-25 11:55:15 +0300

    fix: preloader style

--- src/index.js
+++ src/index.js
@@ -31,5 +31,6 @@ import './styles/media.scss';
 $(document).on('DOMContentLoaded', function() {
     setTimeout(() => {
         $('.js-page-preloader').css('opacity', '0');
+        $('.js-page-preloader').css('display', 'none');
     }, 500)
 })
