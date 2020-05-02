002cf33729b699fe6cc4a1c44da07983d3866857
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2020-05-02 21:57:34 +0300

    refactoring

--- src/components/video-player/VideoPlayer.js
+++ src/components/video-player/VideoPlayer.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 class VideoPlayer {
   constructor(selector) {
     this.$videoContainer = $(selector);
-    this.$videoScreen = this.$videoContainer.find('.js-video-player__screen');
-    this.$videoProgress = this.$videoContainer.find('.js-video-player__control_type_progress');
+    [this.videoScreen] = this.$videoContainer.find('.js-video-player__screen');
+    this.$videoProgress = this.$videoContainer.find('.js-video-player__control-progress-icon');
     this.controls = {
       $play: this.$videoContainer.find('.js-video-player__control_type_play'),
       $pause: this.$videoContainer.find('.js-video-player__control_type_pause'),
@@ -10,69 +11,70 @@ class VideoPlayer {
     };
   }
 
-  bindActions() {
-    this.controls.$play.on('click.controlPlay', this.handlePlayControlClick.bind(this));
-    this.controls.$pause.on('click.controlPause', this.handlePauseControlClick.bind(this));
-    this.controls.$fullScreen.on('click.controlFullscreen', this.handleFullscreenControlClick.bind(this));
-    this.$videoScreen.on('timeupdate.screen', this.handleScreenClick.bind(this));
-    this.$videoProgress.on('click.controlProgress', this.handleProgressControlClick.bind(this));
-  }
-
-  handlePlayControlClick() {
-    this.toggleVideoState();
-  }
-
-  handlePauseControlClick() {
-    this.toggleVideoState();
-  }
-
-  handleProgressControlClick(e) {
-    this.setVideoState(e);
-  }
-
-  handleFullscreenControlClick() {
-    this.$videoScreen[0].requestFullscreen();
-  }
-
-  handleScreenClick() {
-    this.runningProgressBar(this.$videoScreen[0].currentTime, this.$videoScreen[0].duration);
-  }
-
   setCurrentTime(time) {
-    this.$videoScreen[0].currentTime = time;
+    this.videoScreen.currentTime = time;
   }
 
-  toggleVideoState() {
-    if (this.$videoScreen[0].paused) {
-      this.$videoScreen[0].play();
+  _bindActions() {
+    this.controls.$play.on('click.controlPlay', this._handlePlayControlClick.bind(this));
+    this.controls.$pause.on('click.controlPause', this._handlePauseControlClick.bind(this));
+    this.controls.$fullScreen.on('click.controlFullscreen', this._handleFullscreenControlClick.bind(this));
+    this.$videoProgress.on('click.controlProgress', this._handleControlProgressIconClick.bind(this));
+    this.videoScreen.addEventListener('timeupdate', this._handleScreenTimeUpdate.bind(this));
+  }
+
+  _handlePlayControlClick() {
+    this._toggleVideoState();
+  }
+
+  _handlePauseControlClick() {
+    this._toggleVideoState();
+  }
+
+  _handleControlProgressIconClick(e) {
+    this._setVideoState(e);
+  }
+
+  _handleFullscreenControlClick() {
+    this.videoScreen.requestFullscreen();
+  }
+
+  _handleScreenTimeUpdate() {
+    this._runningProgressBar(this.videoScreen.currentTime, this.videoScreen.duration);
+  }
+
+  _toggleVideoState() {
+    if (this.videoScreen.paused) {
+      this.videoScreen.play();
       this.controls.$play.hide();
       this.controls.$pause.show();
     } else {
-      this.$videoScreen[0].pause();
+      this.videoScreen.pause();
       this.controls.$play.show();
       this.controls.$pause.hide();
     }
   }
 
-  runningProgressBar(currentTime, duration) {
-    this.$videoProgress[0].value = 100 * (currentTime / duration);
+  _runningProgressBar(currentTime, duration) {
+    this.$videoProgress.val(100 * (currentTime / duration));
   }
 
-  setVideoState(event) {
+  _setVideoState(event) {
     this.controls.$play.hide();
     this.controls.$pause.show();
-    const w = this.$videoProgress.width();
-    const o = event.offsetX;
-    const d = this.$videoScreen[0].duration;
 
-    this.$videoProgress[0].value = 100 * (o / w);
-    this.$videoScreen[0].pause();
-    this.$videoScreen[0].currentTime = d * (o / w);
-    this.$videoScreen[0].play();
+    const progressIconWidth = this.$videoProgress.width();
+    const eventOffset = event.offsetX;
+    const videoDuration = this.videoScreen.duration;
+    this.$videoProgress.val(100 * (eventOffset / progressIconWidth));
+
+    this.videoScreen.pause();
+    this.videoScreen.currentTime = videoDuration * (eventOffset / progressIconWidth);
+    this.videoScreen.play();
   }
 
   init() {
-    this.bindActions();
+    this._bindActions();
     this.controls.$pause.hide();
   }
 }
